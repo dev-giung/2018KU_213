c0e368ef45e591fdff5b108229d6329b538c1602
dev-giung <giung1021@gmail.com>
2018-04-11 11:36:33 +0900

    asdf

--- HW02/hw02_pro1.c
+++ HW02/hw02_pro1.c
@@ -18,14 +18,49 @@ typedef struct student {
 } student;
 
 /*
-	function: 이중연결리스트에 요소를 삽입
+	function: 이중연결리스트를 초기화 
 	Note	: 
+	input	: 
+	output	: (NULL)
+*/
+void init(student *phead) {
+	
+	(phead -> llink) = phead;
+	(phead -> rlink) = phead;
+	phead -> score = 100;
+}
+
+/*
+	function: 이중연결리스트에 요소를 삽입
+	Note	: 요소들의 score 값이 내림차순이 되도록 삽입함. 
 	input	: 
 	output	: 
 */
-void dinsert_node() {
+void dinsert_node(student *before, student *new_node) {
 	
-	// something
+	while(1) {
+		if(before -> rlink == before) {
+			break;
+		}
+		if(new_node -> score > before -> rlink -> score) {
+			break;
+		} else {
+			before = before -> rlink;
+		}
+	}
+	
+	( new_node -> llink ) = before;
+	( new_node -> rlink ) = ( before -> rlink );
+	( ( before -> rlink ) -> llink ) = new_node;
+	( before -> rlink ) = new_node;
+}
+
+void display(student *phead) {
+	student *p;
+	for(p = phead -> rlink; p != phead; p = p -> rlink) {
+		printf("< |%x| %d |%x| >\n", p -> llink, p -> score, p -> rlink);
+	}
+	printf("\n");
 }
 
 /*
@@ -68,7 +103,29 @@ int main() {
 		return 1;
 	}
 	
-	// something
+	student head_node;
+	student *data[10];
+	int i;
+	
+	init(&head_node);
+	display(&head_node);
+	
+	data[0] = (student *)malloc(sizeof(student));
+	data[0] -> score = 10;
+	dinsert_node(&head_node, data[0]);
+	display(&head_node);
+	
+	data[1] = (student *)malloc(sizeof(student));
+	data[1] -> score = 20;
+	dinsert_node(&head_node, data[1]);
+	display(&head_node);
+	
+	data[2] = (student *)malloc(sizeof(student));
+	data[2] -> score = 5;
+	dinsert_node(&head_node, data[2]);
+	display(&head_node);
+	
+	display(&head_node);
 	
 	// 프로그램 종료 
 	return 0;
